e9058323d7bd883d40ea3f3fb7140fe58924cafd
Илья Богдан <kadetizomska111@gmail.com>
2024-12-07 13:46:31 +0600

    SDL linked like submodule

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
+#include "SDL.h"
 #include "Kernel/Engine.hpp"
 
+void drawWindow() {
+    SDL_Log("%s", "Hello world!");
+}
+
 int main() {
+    drawWindow();
     Kernel::Engine engine = Kernel::Engine();
     while (true) {
         if (engine.mainLoop()) {
